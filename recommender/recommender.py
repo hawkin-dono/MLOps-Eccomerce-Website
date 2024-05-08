30a0e4929f6760b2f38b1e816d527f7c99e35a78
Khong Ngoc Anh <khongngocanh2004@gmail.com>
2024-05-09 01:53:23 +0700

    updated recommendation system & top similarities

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -21,30 +21,56 @@ class RecommendationSystem(object):
         self.rating_matrix = None
         self.similarities_matrix_file = similarities_matrix_file
         self.similarities_matrix = None
-        self.new_users = None
+        self.min_rating_counts = 1
         self.senior_users = None
+        self.item_lists = None
 
     def read_csv(self, file):
+        """Read the rating data from csv
+
+        Args:
+            file (.csv): The file
+        """
         self.rating_data = pd.read_csv(file)
 
     def matrix_construction(self):
+        """Generate the rating matrix
+
+        Returns:
+            pd.DataFrame: Rating matrix
+        """
         # Create a pivot table
         pivot_table = pd.pivot_table(self.rating_data, values='rating', index='customer_id', columns='product_id')
         # Only retain users that voted for at least 10 items
         pivot_table['cnt'] = pivot_table.count(axis=1)
-        self.senior_users = pivot_table[pivot_table['cnt'] >= 10].index.to_list()
-        pivot_table = pivot_table[pivot_table['cnt'] >= 10].drop(columns=['cnt'])
+        self.senior_users = pivot_table[pivot_table['cnt'] >= self.min_rating_counts].index.to_list()
+        self.item_lists = pivot_table.drop(columns=['cnt']).columns.to_list()
+        pivot_table = pivot_table[pivot_table['cnt'] >= self.min_rating_counts].drop(columns=['cnt'])
         pivot_table = pivot_table.fillna(0)
         self.rating_matrix = pivot_table
         return pivot_table
 
     def high_rating(self):
+        """A list of the highest rating items
+
+        Returns:
+            List: The list
+        """
         sum = self.rating_matrix.sum()
-        count = (self.rating_matrix > 0).astype(int).sum(axis=0)
+        count = (self.rating_matrix > 0).astype(float).sum(axis=0)
         rating = (sum / count).fillna(0).sort_values(ascending=False)
         return rating.head(10).index.to_list()
 
     def adjusted_cosine(self, item_i, item_j):
+        """Calculate the adjusted cosine between items
+
+        Args:
+            item_i (int): The item's index 
+            item_j (int): The item's index
+
+        Returns:
+            double: The adjusted cosine similarity
+        """
         rated_users = self.rating_matrix[
             (self.rating_matrix.iloc[:, item_i] > 0) & (self.rating_matrix.iloc[:, item_j] > 0)]
         numerator = 0
@@ -67,28 +93,48 @@ class RecommendationSystem(object):
         return numerator / denominator
 
     def similarities(self):
+        """Generate the similarities matrix
+
+        Returns:
+            pd.DataFrame: The similarities matrix
+        """
         if self.similarities_matrix_file is not None:
             matrix = pd.read_csv(self.similarities_matrix_file)
             self.similarities_matrix = matrix
             return matrix
 
         n_items = len(self.rating_matrix.columns)
-        print(n_items)
         matrix = np.zeros((n_items, n_items))
         for item_i in tqdm(range(n_items)):
             for item_j in range(n_items):
                 matrix[item_i, item_j] = self.adjusted_cosine(item_i, item_j)
         pd.DataFrame(matrix).to_csv('similarities_matrix.csv', index=False)
+        self.similarities_matrix = pd.DataFrame(matrix)
         return matrix
 
+    def most_similar(self, iid):
+        item_i = self.item_lists.index(iid)
+        most_similar = self.similarities_matrix.iloc[item_i].sort_values(ascending=False)
+        item_indexes =  most_similar.head(10).index.astype(int).to_list()
+        return [self.item_lists[index] for index in item_indexes]
+
     def predict(self, uid, item_i):
+        """Predict the rating of a user for an item
+
+        Args:
+            uid (int): The user's id 
+            item_i (int): The item's index in the item list. Shall be fix later
+
+        Returns:
+            double: The predicted rating
+        """
         if uid not in self.senior_users:
             return None
         user_u = self.senior_users.index(uid)
-        user_rating = self.rating_matrix.iloc[user_u]
+        user_rating = self.rating_matrix.iloc[user_u].to_list()
         numerator = 0
         denominator = 0
-        for item_id in user_rating.index:
+        for item_id in range(len(self.item_lists)):
             if user_rating[item_id] == 0.:
                 continue
             numerator += abs(self.similarities_matrix.iloc[item_id, item_i] * user_rating[item_id])
@@ -98,8 +144,15 @@ class RecommendationSystem(object):
         return numerator / denominator
 
     def recommend(self, uid):
+        """ Recommend items for a user 
+
+        Args:
+            uid (int): user's id
+
+        Returns:
+            list: A list of recommended items
+        """
         if uid not in self.senior_users:
-            print('Nani????')
             return self.high_rating()
         user_u = self.senior_users.index(uid)
         recommend = []
@@ -109,13 +162,14 @@ class RecommendationSystem(object):
                 recommend.append((self.predict(uid, item_i), item_i))
         recommend.sort(reverse=True)
         recommend = recommend[:10]
-        return [rcm[1] for rcm in recommend]
+        return [self.item_lists[rcm[1]] for rcm in recommend]
 
 
 if __name__ == '__main__':
     rcm = RecommendationSystem(similarities_matrix_file='similarities_matrix.csv')
-    rcm.read_csv('user_item_rating.csv')
+    rcm.read_csv('processed_data/comments_data_bach_hoa_online.csv')
     rcm.matrix_construction()
     rcm.similarities()
-    print(rcm.recommend(2))
+    print(rcm.most_similar(15973974))
+    # print(rcm.similarities_matrix.iloc[1].value_counts())
 
